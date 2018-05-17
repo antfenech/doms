a5ed59e6e34be45182c06ecb64d4726a37574f34
Anthony Fenech <antfenech@yahoo.com>
2018-05-16 21:21:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charSet="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dom's Noms</title>
+</head>
+<body>
+  <div className="main">
+    <h1>Dom's Nom's</h1>
+    <h3>Coming Soon</h3>
+
+    <h4>Cell: (650) 868-7531</h4>
+  </div>
+</body>
+</html>
